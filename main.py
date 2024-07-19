9ddafe7744072d9fb48151fa84e7a47f650bf0b8
York Xiang <bombless@126.com>
2024-07-20 02:37:03 +0800

    Local adjustment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import librosa
 import glob
+import sounddevice as sd
 
 import read_with_numpy
 
@@ -24,6 +25,26 @@ def read(file):
     print('max_value:', '{:.3f}'.format(max_value))
     print('max_value time:', format_time(max_index / sample_rate))
 
+    left_index = max_index - sample_rate * 10
+    right_index = max_index + sample_rate * 10
+
+    divide_right = right_index
+
+    print('playing...')
+    sd.play(data[left_index: right_index], sample_rate, blocking=True)
+
+    left_max_index = read_with_numpy.arg_max(data[:left_index])
+    left_index = left_max_index - sample_rate * 10
+    right_index = left_max_index + sample_rate * 10
+    print('playing max value on the left side... time:', format_time(left_max_index / sample_rate))
+    sd.play(data[left_index: right_index], sample_rate, blocking=True)
+
+    right_max_index = divide_right + read_with_numpy.arg_max(data[divide_right:])
+    left_index = right_max_index - sample_rate * 10
+    right_index = right_max_index + sample_rate * 10
+    print('playing max value on the right side... time:', format_time(right_max_index / sample_rate))
+    sd.play(data[left_index: right_index], sample_rate, blocking=True)
+
     end_round_time = time.time()
     round_time = end_round_time - start_time
     print('round_time: {:.2f} seconds'.format(round_time))
@@ -35,10 +56,11 @@ def format_time(seconds):
 
 def main():
     start_time = time.time()
-    file_list = glob.glob('e:/20231025/record-*.wav')
-    for file in file_list:
-        print(file, ':')
-        read(file)
+    # file_list = glob.glob('e:/2023*/record-*.wav')
+    # for file in file_list:
+    #     print(file, ':')
+    #     read(file)
+    read('f:/h.wav')
     end_time = time.time()
     total_time = end_time - start_time
     print('total_time: {:.2f} seconds'.format(total_time))
